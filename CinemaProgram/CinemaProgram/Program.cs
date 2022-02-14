8cca97e93d99b7dab58c1ec61d7bfa6c40d41fbe
Avexces <46761876+Avexces@users.noreply.github.com>
2022-02-14 14:17:48 +0100

    Empty Program
    what he said

---
+++ CinemaProgram/CinemaProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CinemaProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
